693c674ec53de73e3f1f0c9a91442dd7dffbc5a7
Shaikh-Ubaid <shaikhubaid769@gmail.com>
2020-06-30 22:14:53 +0530

    added one more product and separated data from main script

---
+++ sitedata.js
@@ -0,0 +1,247 @@
+var SiteData = {
+    links: [
+        {
+            id: 1,
+            title: "Home",
+            href: "#banner"
+        },
+        {
+            id: 2,
+            title: "Products",
+            href: "#our-products"
+        },
+        {
+            id: 3,
+            title: "About",
+            href: "#about-us"
+        },
+        {
+            id: 4,
+            title: "Contact",
+            href: "#contact-us"
+        }],
+    header:
+    {
+        title: "Kinara Publications",
+        quote: "Delievering More than expected",
+        info: "",
+        image: "img/banner-img.jpg"
+    },
+    productdetails:
+    {
+        name: "Our Products",
+        description: "We print all types of books",
+        products: [
+            {
+                name: "Pupils Attendance Register's",
+                image: "https://images.pexels.com/photos/1392854/pexels-photo-1392854.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            },
+            {
+                name: "Teacher Muster Roll",
+                image: "https://images.pexels.com/photos/1340588/pexels-photo-1340588.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            },
+            {
+                name: "Cumulative Record File",
+                image: "https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            },
+            {
+                name: "Students Daily Dairy",
+                image: "https://i1.pickpik.com/photos/3/729/276/book-rose-flower-wedding-table-preview.jpg"
+            },
+            {
+                name: "Lesson Note Book",
+                image: "https://p0.pikrepo.com/preview/672/64/red-heart-on-black-book-thumbnail.jpg"
+            },
+            {
+                name: "Progress Report Cards",
+                image: "https://p0.pikrepo.com/preview/207/815/several-pens-on-white-book-page-thumbnail.jpg"
+            }],
+        extraproducts: [
+            {
+                name: "Exam Register For I to VIII",
+                image: ""
+            },
+            {
+                name: "Evaluation Register",
+                image: ""
+            },
+            {
+                name: "Printed Answer Sheet",
+                image: ""
+            },
+            {
+                name: "School Receipt Book",
+                image: ""
+            },
+            {
+                name: "Stickers",
+                image: ""
+            },
+            {
+                name: "Bill Book",
+                image: ""
+            },
+            {
+                name: "Letter Pad",
+                image: ""
+            },
+            {
+                name: "Visiting Card",
+                image: ""
+            },
+            {
+                name: "Wedding Card",
+                image: ""
+            },
+            {
+                name: "Mfg. of Note Books",
+                image: ""
+            },
+            {
+                name: "Identity Card",
+                image: ""
+            },
+            {
+                name: "Question Papers 1st to 8th",
+                image: ""
+            },
+            {
+                name: "All Schools & Office Stationary",
+                image: ""
+            },
+            {
+                name: "College Record Book (practical book)",
+                image: ""
+            }
+        ],
+        extra: false
+    },
+    qualitydetails:
+    {
+        name: "Quality Assured",
+        description: "We believe in Quality Work",
+        statements: [
+            {
+                name: "Reasonable Price"
+            },
+            {
+                name: "No Compromise in Quality"
+            },
+            {
+                name: "Customer Satisfaction - Our Utmost Priority"
+            },]
+    },
+    factdetails:
+    {
+        name: "Facts",
+        description: "Have a look at our statistics",
+        facts: [
+            {
+                index: 0,
+                name: "Books Printed",
+                count: "0",
+                startValue: 0,
+                endValue: 12453
+            },
+            {
+                index: 1,
+                name: "Customers Served",
+                count: "0",
+                startValue: 0,
+                endValue: 4025
+            },
+            {
+                index: 2,
+                name: "Projects Completed",
+                count: "0",
+                startValue: 0,
+                endValue: 2520
+            },],
+        flag: true,
+    },
+    testimonialdetails:
+    {
+        name: "Testimonials",
+        description: "See what our customers are saying",
+        testimonials: [
+            {
+                id: 0,
+                name: "Shaikh Ubaid",
+                comment: "Very nice Publication",
+                img: "img/img1.jpeg"
+            },
+            {
+                id: 1,
+                name: "Kafeel Ahmed",
+                comment: "Best Quality with affordable price",
+                img: "img/img4.jpeg"
+            },
+            {
+                id: 2,
+                name: "Jawed Sir",
+                comment: "Very Satisfied with the Quality. Thank you Kinara",
+                img: "img/img3.jpeg"
+            }]
+    },
+    info:
+    {
+        name: "About Us",
+        description: "Few words about us",
+        details: `Kinara Publications is a hard-working company dedicated towards providing premium quality stationary items at minimum possible rates.`,
+        img: "img/about-us.jpg",
+        quote: "We are now one of the most trusted publishing house with many of our titles recommended as text books by universities."
+    },
+    contact:
+    {
+        name: "Contact Us",
+        description: "We are eager to hear from you",
+        subheading: "Get In Touch",
+        email: "kinarapublications@gmail.com",
+        whatsappno: "+919764444408,+917588930502",
+        address: "Near V I P Function Hall, beside Sahara Hospital central naka road",
+        city: "Aurangabad",
+        pincode: "431001",
+        state: "Maharashtra"
+    },
+    footer:
+    {
+        socialMedia: [
+            {
+                id: 0,
+                name: "googleplus",
+                class: "fa-google-plus-square",
+                href: ""
+            },
+            {
+                id: 1,
+                name: "twitter",
+                class: "fa-twitter-square",
+                href: ""
+            },
+            {
+                id: 2,
+                name: "facebook",
+                class: "fa-facebook-official",
+                href: ""
+            },
+            {
+                id: 3,
+                name: "youtube",
+                class: "fa-youtube-play",
+                href: ""
+            },
+            {
+                id: 4,
+                name: "instagram",
+                class: "fa-instagram",
+                href: ""
+            },],
+        header: "Office",
+        address: [
+            "Near V I P Function Hall",
+            "beside Sahara Hospital central naka road",
+            "Aurangabad 431001",
+            "Maharashtra"
+        ]
+    }
+}
